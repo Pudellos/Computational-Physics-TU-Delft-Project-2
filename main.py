2c3d5e2f09bf21ef5342cad35cd24d87ac3adef2
Nico Kerkhoven <n.c.kerkhoven@student.tudelft.nl>
2021-04-20 12:09:09 +0200

    Added the scaling of r^2 and the errorbars
    im not completely sure if the errorbars are working properly and the efficiency could propably still be somewhat improved

--- main.py
+++ main.py
@@ -40,3 +40,31 @@ if(sys.argv[1] == "r_sq_example"):
     avg_r_sq = sum([weights[i] * r_sq[i] for i in range(N)]) / sum(weights)
 
     print(avg_r_sq)
+    
+# If you want to run this, run this in the terminal -> python main.py r_sq_vs_L
+if(sys.argv[1] == "r_sq_vs_L"):
+    N = 100
+    L = 50
+    avg_r_sq= np.zeros(L)
+    for l in range(L):
+        weights = np.zeros(N)
+        r_sq = np.zeros(N)
+        for i in range(N):
+                box = np.zeros(shape=(3 * L, 3 * L))
+                polymer = Polymer(l, [L, L], box)
+                weights[i] = polymer.weight
+                r_sq[i] = polymer.end_to_end_dist()**2       
+        avg_r_sq[l] = sum(weights* r_sq) / sum(weights)
+        s[l]=((N/(N-1))*(sum(((weights)**2)*(r_sq-avg_r_sq[l])**2))/(sum(weights)**2))**(1/2)
+    #I have not yet found out how to plot the figure within the terminal.
+    length=np.arange(0,L)
+    plt.figure(1)
+    plt.errorbar(length,avg_r_sq,s, label = 'Simulated average $r^2$')
+    plt.plot(length,length**(3/2), label = 'L^(3/2)')
+    plt.xlabel('L')
+    plt.ylabel('Radius of gyration')
+    plt.legend()
+    plt.show()
+        
+
+        
